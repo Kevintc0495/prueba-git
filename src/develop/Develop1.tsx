491882622a0e4c9b648998e35f5aef3f8c987662
kevintc04951 <kevintc0495@gmail.com>
2023-08-03 00:22:22 -0500

    Componente Develop1: Se agrega parrafo

--- src/develop/Develop1.tsx
+++ src/develop/Develop1.tsx
@@ -4,6 +4,7 @@ const Develop1 = () => {
   return (
     <>
       <div>Develop1</div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora harum fugiat culpa quasi provident tempore deserunt qui, eveniet facilis magnam minus saepe molestias sed, est optio ipsum nesciunt eum a.</p>
       <button>D1</button>
     </>
   )
